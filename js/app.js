f90381c02e87da7c913307d00a0d721b91ad4d1f
Joey Mudd <jmudd@virtualpeaker.io>
2022-05-19 08:38:21 -0400

    Add form and connect it to js function to demonstrate conditionals

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 console.log('We Made It!!!');
+
+function handleForm() {
+  const subject = document.getElementById('subject').value;
+  const firstName= document.getElementById('firstName').value;
+  const lastName = document.getElementById('lastName');
+  const message = document.getElementById('message');
+
+  if (subject.toLowerCase() === 'math') {
+    message.innerHTML = firstName + ' ' + lastName.value + ': please go to room 201';
+  } else if (subject === 'history') {
+    message.innerHTML = firstName + ' ' + lastName.value + ': please go to room 405';
+  } else {
+    message.innerHTML = 'Sorry, ' + firstName + ': we dont have that class';
+  }
+}
+
+document.getElementById('formSubmit').addEventListener('click', handleForm);
